b461c3d2d616f5a273c4588b9ab53f2915ad0ce4
Mark <kevzlou7979@gmail.com>
2015-10-22 16:20:48 -0700

    Move demo to another project insclix.ui.gwt.demo

--- insclix.ui.gwt/src/insclix/ui/gwt/client/Insclix_ui_gwt.java
+++ insclix.ui.gwt/src/insclix/ui/gwt/client/Insclix_ui_gwt.java
@@ -1,9 +1,6 @@
 package insclix.ui.gwt.client;
 
-import insclix.ui.gwt.client.demo.Demo;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -12,6 +9,5 @@ public class Insclix_ui_gwt implements EntryPoint {
 	
 	public void onModuleLoad() {
 		
-		RootPanel.get().add(new Demo());
 	}
 }
